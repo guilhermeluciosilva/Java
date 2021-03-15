59249741d5662bb234110bdade5bcfaf231c2040
Guilherme Lucio Silva <61800391+guilhermeluciosilva@users.noreply.github.com>
2021-03-15 14:47:48 -0300

    Create ex02.java

---
+++ ex02.java
@@ -0,0 +1,40 @@
+package domain;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        
+        // + Operador de Soma
+        
+        int valor1 = 10;
+        int valor2 = 10;
+        
+        int soma = valor1 + valor2;
+        
+        // - Operador de Subtração
+        
+        int valor3 = 20;
+        int valor4 = 10;
+        
+        int subtracao = valor3 - valor4;
+        
+        // / Operador de Divisão
+        
+        int valor5 = 20;
+        int valor6 = 10;
+        
+        int divicao = valor5 / valor6;
+        
+        // * Operador de multiplicação
+        
+        int valor7 = 10;
+        int valor8 = 10;
+        
+        int multiplicacao = valor7 * valor8;
+        
+        
+        
+        System.out.println(multiplicacao);
+    }
+
+}
