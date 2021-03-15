94c417cce91d291626b5dbc983d5882755af461e
Guilherme Lucio Silva <61800391+guilhermeluciosilva@users.noreply.github.com>
2021-03-15 15:11:34 -0300

    Create ex03.java

---
+++ ex03.java
@@ -0,0 +1,28 @@
+package domain;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        
+        /*
+        == Operador de Igual
+        < Operador de Menor
+        > Operador de Maior
+        <= Operador de Menor ou igual
+        >= Operador de Maior ou igual
+        != Operador de diferente
+        
+        */
+        
+        int valor1 = 20;
+        int valor2 = 10;
+        
+        System.out.println(valor1 == valor2);
+        System.out.println(valor1 < valor2);
+        System.out.println(valor1 > valor2);
+        System.out.println(valor1 <= valor2);
+        System.out.println(valor1 >= valor2);
+        System.out.println(valor1 != valor2);
+    }
+
+}
