661ca15c2c8fbf66169ce5382a51a893b4c0357a
Guilherme Lucio Silva <61800391+guilhermeluciosilva@users.noreply.github.com>
2021-03-15 17:03:46 -0300

    Create ex04.java

---
+++ ex04.java
@@ -0,0 +1,30 @@
+package domain;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        
+        if(false){
+            System.out.println("Verdadeiro");
+        }else{
+            System.out.println("Falso");
+        }
+        
+        
+        String sexo = "Feminino";
+        
+        if(sexo == "Masculino"){
+            
+            System.out.println("Sexo Masculino");
+            
+        }else if(sexo == "Feminino"){
+            
+            System.out.println("Feminino");
+            
+        }
+        
+        
+        
+    }
+
+}
